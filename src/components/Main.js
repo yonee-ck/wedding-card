bc5f01e8aec625d48eea2d36a5069e546ba33c74
김연희 <yonee@ogqcorp.com>
2021-10-13 19:55:21 +0900

    초기 구성 완료

---
+++ src/components/Main.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Form(props) {
+    return (
+        <form className='container'>
+            <div className='main'>
+                <img className='main--img' src='https://media.tenor.com/images/0a1652de311806ce55820a7115993853/tenor.gif'  alt='sss'/>
+            </div>
+            {/*<link*/}
+            {/*    href="https://fonts.googleapis.com/css?family=Noto+Sans+KR:100,300,400,500,700,900&display=swap&subset=korean"*/}
+            {/*    rel="stylesheet"*/}
+            {/*/>*/}
+            {/*<meta name='viewport' height={devicePixelRatio} />*/}
+            {/*<img source={TestGif} />*/}
+            {/*<h2 className="label-wrapper">*/}
+                {/*<label htmlFor="new-todo-input" className="label__lg">*/}
+                {/*    What needs to be done?*/}
+                {/*</label>*/}
+            {/*</h2>*/}
+            {/*<input*/}
+            {/*    type="text"*/}
+            {/*    id="new-todo-input"*/}
+            {/*    className="input input__lg"*/}
+            {/*    name="text"*/}
+            {/*    autoComplete="off"*/}
+            {/*/>*/}
+            {/*<button type="submit" className="btn btn__primary btn__lg">*/}
+            {/*    Add*/}
+            {/*</button>*/}
+        </form>
+    );
+}
+
+export default Form;
