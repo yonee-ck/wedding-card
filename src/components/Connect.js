7a2b31b995f211908fe20fe7b2f53f5babc811e3
김연희 <yonee@ogqcorp.com>
2021-10-16 19:53:10 +0900

    favicon 변경, connect 수정

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -30,9 +30,10 @@ class Connect extends Component {
                             <CopyToClipboard onCopy={this.handleCopy} text='59062501048264'>
                                 <span className='information'>&#127974;&nbsp;&nbsp;국민&nbsp;&nbsp;5906-250104-8264</span>
                             </CopyToClipboard>
-                            <span className='information'>&#128172;&nbsp;&nbsp;와주셔서 감사합니다~!!</span>
                         </div>
                     </div>
+                    <span className='information separation'>&#128150;&nbsp;&nbsp;김준수, 스테이씨, (구)동방신기, JYJ, 와조스키</span>
+                    <span className='information separation end'>&#128172;&nbsp;&nbsp;와주셔서 감사합니다~!!</span>
                     <div className='section__connect__group top'>
                         <a href='tel:01064392034' className='call'><img className='icon--connect' src={CallImg} alt='통화하기' /></a>
                         <a href='sms:01064392034' className='call'><img className='icon--connect' src={MessageImg} alt='메시지 보내기'/></a>
@@ -40,16 +41,17 @@ class Connect extends Component {
                 </div>
                 <div className='section-card'>
                     <div className='section__profile'>
-                        <img className='section__profile--img' src={YGImg}  alt='권용'/>
+                        <img className='section__profile--img' src={YGImg}  alt='권용근'/>
                         <div className='section__profile--row'>
                             <span className='name'><span className='nickname'>신랑</span>권용근</span>
                             <span className='information'>&#128222;&nbsp;&nbsp;010-3775-7341</span>
-                            <CopyToClipboard onCopy={this.handleCopy} text='59062501048264'>
-                                <span className='information'>&#127974;&nbsp;&nbsp;신한&nbsp;&nbsp;5906-250104-8264</span>
+                            <CopyToClipboard onCopy={this.handleCopy} text='05206335701014'>
+                                <span className='information'>&#127974;&nbsp;&nbsp;기업&nbsp;&nbsp;052-063357-01-014</span>
                             </CopyToClipboard>
-                            <span className='information'>&#128172;&nbsp;&nbsp;와주셔서 감사합니다~!!</span>
                         </div>
                     </div>
+                    <span className='information separation'>&#128150;&nbsp;&nbsp;개발, 전자제품</span>
+                    <span className='information separation end'>&#128172;&nbsp;&nbsp;와주셔서 감사합니다~!!</span>
                     <div className='section__connect__group top'>
                         <a href='tel:01037757341' className='call'><img className='icon--connect' src={CallImg} alt='통화하기' /></a>
                         <a href='sms:01037757341' className='call'><img className='icon--connect' src={MessageImg} alt='메시지 보내기'/></a>
@@ -61,33 +63,33 @@ class Connect extends Component {
                         <img className='section__profile--img' src={YGImg}  alt='권순갑'/>
                         <div className='section__profile--row'>
                             <span className='name'><span className='nickname'>신랑측 혼주</span>권순갑</span>
-                            <span className='information'>&#128222;&nbsp;&nbsp;010-3775-7341</span>
-                            <CopyToClipboard onCopy={this.handleCopy} text='59062501048264'>
-                                <span className='information'>&#127974;&nbsp;&nbsp;신한&nbsp;&nbsp;5906-250104-8264</span>
+                            <span className='information'>&#128222;&nbsp;&nbsp;010-8733-1857</span>
+                            <CopyToClipboard onCopy={this.handleCopy} text='1002951593148'>
+                                <span className='information'>&#127974;&nbsp;&nbsp;우리&nbsp;&nbsp;100-29515-93-148</span>
                             </CopyToClipboard>
-                            <span className='information'>&#128172;&nbsp;&nbsp;♡ 축하와 응원으로 축복해주세요 ♡</span>
                         </div>
                     </div>
+                    <span className='information separation'>&#128172;&nbsp;&nbsp;축하와 응원으로 축복해주세요~</span>
                     <div className='section__connect__group top'>
-                        <a href='tel:01037757341' className='call'><img className='icon--connect' src={CallImg} alt='통화하기' /></a>
-                        <a href='sms:01037757341' className='call'><img className='icon--connect' src={MessageImg} alt='메시지 보내기'/></a>
+                        <a href='tel:01087331857' className='call'><img className='icon--connect' src={CallImg} alt='통화하기' /></a>
+                        <a href='sms:01087331857' className='call'><img className='icon--connect' src={MessageImg} alt='메시지 보내기'/></a>
                     </div>
                 </div>
                 <div className='section-card'>
                     <div className='section__profile'>
-                        <img className='section__profile--img' src={JHSImg}  alt='정현'/>
+                        <img className='section__profile--img' src={JHSImg}  alt='정현숙'/>
                         <div className='section__profile--row'>
                             <span className='name'><span className='nickname'>신랑측 혼주</span>정현숙</span>
-                            <span className='information'>&#128222;&nbsp;&nbsp;010-3775-7341</span>
-                            <CopyToClipboard onCopy={this.handleCopy} text='59062501048264'>
-                                <span className='information'>&#127974;&nbsp;&nbsp;신한&nbsp;&nbsp;5906-250104-8264</span>
+                            <span className='information'>&#128222;&nbsp;&nbsp;010-3804-1857</span>
+                            <CopyToClipboard onCopy={this.handleCopy} text='06928256812001'>
+                                <span className='information'>&#127974;&nbsp;&nbsp;우리&nbsp;&nbsp;069-282568-12-001</span>
                             </CopyToClipboard>
-                            <span className='information'>&#128172;&nbsp;&nbsp;♡ 친지분들과 여러분들 모시고 화촉을 밝히게 되었음을 알리오니 두사람 축복해주세요 ♡</span>
                         </div>
                     </div>
+                    <span className='information separation'>&#128172;&nbsp;&nbsp;친지분들과 여러분들 모시고 화촉을 밝히게 되었음을 알리오니 두사람 축복해주세요.</span>
                     <div className='section__connect__group top'>
-                        <a href='tel:01037757341' className='call'><img className='icon--connect' src={CallImg} alt='통화하기' /></a>
-                        <a href='sms:01037757341' className='call'><img className='icon--connect' src={MessageImg} alt='메시지 보내기'/></a>
+                        <a href='tel:01038041857' className='call'><img className='icon--connect' src={CallImg} alt='통화하기' /></a>
+                        <a href='sms:01038041857' className='call'><img className='icon--connect' src={MessageImg} alt='메시지 보내기'/></a>
                     </div>
                 </div>
                 <div className='section-card'>
@@ -96,9 +98,9 @@ class Connect extends Component {
                         <div className='section__profile--row'>
                             <span className='name'><span className='nickname'>신부측 혼주</span>조필호</span>
                             <span className='information'>&#128222;&nbsp;&nbsp;010-9064-8989</span>
-                            <span className='information'>&#128172;&nbsp;&nbsp;선남선녀 한쌍의 경이로운 탄생의 현장으로 소중한분들을 초대합니다~</span>
                         </div>
                     </div>
+                    <span className='information separation'>&#128172;&nbsp;&nbsp;선남선녀 한쌍의 경이로운 탄생의 현장으로 소중한분들을 초대합니다~</span>
                     <div className='section__connect__group top'>
                         <a href='tel:01090648989' className='call'><img className='icon--connect' src={CallImg} alt='통화하기' /></a>
                         <a href='sms:01090648989' className='call'><img className='icon--connect' src={MessageImg} alt='메시지 보내기'/></a>
@@ -113,9 +115,9 @@ class Connect extends Component {
                             <CopyToClipboard onCopy={this.handleCopy} text='59062501048264'>
                                 <span className='information'>&#127974;&nbsp;&nbsp;국민&nbsp;&nbsp;073001-04-234523</span>
                             </CopyToClipboard>
-                            <span className='information'>&#128172;&nbsp;&nbsp;사랑하는 저희 딸이 이쁜 새가정을 꾸미려고 합니다.<br/>바쁘시겠지만 함께해주시면 감사하겠습니다.</span>
                         </div>
                     </div>
+                    <span className='information separation'>&#128172;&nbsp;&nbsp;사랑하는 저희 딸이 이쁜 새가정을 꾸미려고 합니다. 바쁘시겠지만 함께해주시면 감사하겠습니다.</span>
                     <div className='section__connect__group top'>
                         <a href='tel:01037820519' className='call'><img className='icon--connect' src={CallImg} alt='통화하기' /></a>
                         <a href='sms:01037820519' className='call'><img className='icon--connect' src={MessageImg} alt='메시지 보내기'/></a>
@@ -127,18 +129,22 @@ class Connect extends Component {
                         <img className='section__profile--img' src={OZImg}  alt='오'/>
                         <div className='section__profile--row'>
                             <span className='name'><span className='nickname'>고양이</span>오조</span>
-                            <span className='information'>&#128172;&nbsp;&nbsp;누나, 형아 결혼식에 초대한다옹~</span>
+                            <span className='information'>&#128571;&nbsp;&nbsp;참치회, 캣글라스, 연희누나</span>
+                            <span className='information'>&#128574;&nbsp;&nbsp;낯선 생명체</span>
                         </div>
                     </div>
+                    <span className='information separation'>&#128172;&nbsp;&nbsp;누나, 형아의 결혼식에 초대한다옹~</span>
                 </div>
                 <div className='section-card'>
                     <div className='section__profile'>
                         <img className='section__profile--img' src={DRImg}  alt='도레' />
                         <div className='section__profile--row'>
                             <span className='name'><span className='nickname'>강아지</span>도레</span>
-                            <span className='information'>&#128172;&nbsp;&nbsp;아르르르.. 사회성이 떨어지지만 오늘만은 참아주겠어!</span>
+                            <span className='information'>&#128571;&nbsp;&nbsp;장난감, 간식, 뒹굴기</span>
+                            <span className='information'>&#128574;&nbsp;&nbsp;다른 강아지</span>
                         </div>
                     </div>
+                    <span className='information separation'>&#128172;&nbsp;&nbsp;아르르르.. 사회성이 떨어지지만 오늘만은 참아주겠어!</span>
                 </div>
             </div>
         );
